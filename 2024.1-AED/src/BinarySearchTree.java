2078c712add7ac83afde1624d5677e3973073087
bfsc <brunocartaxo@gmail.com>
2024-03-15 12:06:58 -0300

    -

--- 2024.1-AED/src/BinarySearchTree.java
+++ 2024.1-AED/src/BinarySearchTree.java
@@ -22,23 +22,90 @@ public class BinarySearchTree implements BinaryTree  {
 	}
 	
 	public boolean contains(int value) {
+		return contains(value, this.root);
+	}
+	
+	private boolean contains(int value, Node node) {
+		if (node != null) {
+			if (value == node.value) {
+				return true;
+			} else if (value < node.value) {
+				return contains(value, node.left);
+			} else {
+				return contains(value, node.right);
+			}
+		}
+		
 		return false;
 	}
 
 	public void remove(int value) {
+		this.root = remove(value, this.root);
+	}
+	
+	private Node remove(int value, Node node) {
+		if (node != null) {
+			if (value == node.value) {
+				if (node.left == null && node.right == null) {
+					return null;
+				} else if (node.left != null && node.right != null) {
+					Node inOrderPredecessor = node.left;
+					while (inOrderPredecessor != null) {
+						if (inOrderPredecessor.right == null) {
+							break;
+						}
+						inOrderPredecessor = inOrderPredecessor.right;
+					}
+					
+					node.value = inOrderPredecessor.value;
+					return remove(inOrderPredecessor.value, inOrderPredecessor);
+				} else {
+					return node.left == null ? node.right : node.left;
+				}
+			} else if (value < node.value) {
+				node.left = remove(value, node.left);
+			} else {
+				node.right = remove(value, node.right);
+			}
+		}
 		
+		return node;
 	}
 
 	public void preOrderTraversal() {
-		
+		preOrderTraversal(this.root);
 	}
 
+	private void preOrderTraversal(Node node) {
+		if (node != null) {
+			System.out.println(node.value);
+			preOrderTraversal(node.left);
+			preOrderTraversal(node.right);
+		}
+	}
+	
 	public void inOrderTraversal() {
-		
+		inOrderTraversal(this.root);
 	}
 
+	private void inOrderTraversal(Node node) {
+		if (node != null) {
+			inOrderTraversal(node.left);
+			System.out.println(node.value);
+			inOrderTraversal(node.right);
+		}
+	}
+	
 	public void postOrderTraversal() {
-		
+		postOrderTraversal(this.root);
+	}
+	
+	private void postOrderTraversal(Node node) {
+		if (node != null) {
+			postOrderTraversal(node.left);
+			postOrderTraversal(node.right);
+			System.out.println(node.value);
+		}
 	}
 	
 	private class Node {
--- 2024.1-AED/src/Main.java
+++ 2024.1-AED/src/Main.java
@@ -2,14 +2,17 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Tree bst = new BinarySearchTree();
+		BinaryTree bst = new BinarySearchTree();
 		bst.add(8);
 		bst.add(3);
 		bst.add(20);
 		bst.add(2);
 		bst.add(7);
 		bst.add(5);
-		System.out.println();
+		
+		bst.remove(8);
+		
+		bst.preOrderTraversal();
 	}
 	
 }
